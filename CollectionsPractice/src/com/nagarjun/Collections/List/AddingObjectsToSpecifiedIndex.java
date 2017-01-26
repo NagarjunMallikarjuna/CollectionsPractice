2c600d88c8d7f52dc966ebd5535740b8e63e396b
nagarjun <nagarjunnmn@gmail.com>
2017-01-26 19:15:56 +0530

    test changes

--- CollectionsPractice/src/com/nagarjun/Collections/List/AddingObjectsToSpecifiedIndex.java
+++ CollectionsPractice/src/com/nagarjun/Collections/List/AddingObjectsToSpecifiedIndex.java
@@ -8,8 +8,6 @@ public class AddingObjectsToSpecifiedIndex {
 		ArrayList<String> lst = new ArrayList<String>();
 		lst.add(1, "one");
 		lst.add(5, "five");
-		
-		
 		for(int i=0;i<lst.size()-1;i++){
 			System.out.println(lst.get(i));
 		}
